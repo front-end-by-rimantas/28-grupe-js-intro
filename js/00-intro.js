6b1d6c006813439e1a818525eb3972f3296c402c
Rimantas <belauzas@yahoo.com>
2021-05-04 19:55:33 +0300

    Variables

--- js/00-intro.js
+++ js/00-intro.js
@@ -18,3 +18,8 @@ KONCEPTAI:
 6) oop - objektinis programavimas - tai tik "skonio" reikalas
 
 */
+
+console.log('Labas, ka tu?');
+console.log("Labas, ka tu?");
+console.log(486545);
+console.log('Man yra', 99, 'metai');
