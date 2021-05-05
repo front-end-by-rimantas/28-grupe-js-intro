5ef412e670e01d5be07ade1ff55d97f9b4212965
Rimantas <belauzas@yahoo.com>
2021-05-05 19:15:18 +0300

    Array methods

--- js/04-array.js
+++ js/04-array.js
@@ -1,5 +1,12 @@
 /*
 ARRAY
+
+array metodai:
+- .push()       - prideti i gala nauja reiksme
+- .unshift()    - prideti i prieki nauja reiksme
+- .pop()        - isimti is galo (paskutini)
+- .shift()      - isimti is priekio (pirmaji)
+
 */
 
 const marks = [10, 2, 8, 4, 6, 5, 2, 10, 8];
@@ -18,3 +25,38 @@ const dictionary = ['labas', 'rytas', 'sakau', 'tau', '🚀'];
 
 const paskutinisZodis = dictionary[dictionary.length - 1];
 console.log(paskutinisZodis);
+
+console.log('---------------------');
+
+const abc = ['b', 'c'];
+console.log(abc);
+
+abc.push('d');
+console.log(abc);
+
+abc.shift();
+console.log(abc);
+
+abc.pop();
+console.log(abc);
+
+abc.unshift('a');
+console.log(abc);
+
+console.log(abc.reverse());
+console.log(abc);
+
+console.log('------------------');
+
+const a1 = ['a', 'b'];
+const a2 = ['c', 'd'];
+const a12 = a1.concat(a2)
+console.log(a1);
+console.log(a2);
+console.log(a12);
+
+const modern1 = [...a1, ...a2, ...a12];
+console.log(modern1);
+
+const modern2 = [...a2, ...a1];
+console.log(modern2);
