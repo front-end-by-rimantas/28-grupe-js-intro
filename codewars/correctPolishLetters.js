66acc359bfda26c9e307d320c396e1e0a8ec9e27
Rimantas <belauzas@yahoo.com>
2021-05-08 12:58:11 +0300

    Corrent polish letters with for loop

--- codewars/correctPolishLetters.js
+++ codewars/correctPolishLetters.js
@@ -13,42 +13,20 @@
 function correctPolishLetters(string) {
     let convertedString = '';
 
-    // einame per raides
     for (let i = 0; i < string.length; i++) {
-        // paimu raide
         const raide = string[i];
 
         switch (raide) {
-            case 'ą':
-                convertedString += 'a';
-                break;
-            case 'ć':
-                convertedString += 'c';
-                break;
-            case 'ę':
-                convertedString += 'e';
-                break;
-            case 'ł':
-                convertedString += 'l';
-                break;
-            case 'ń':
-                convertedString += 'n';
-                break;
-            case 'ó':
-                convertedString += 'o';
-                break;
-            case 'ś':
-                convertedString += 's';
-                break;
-            case 'ó':
-                convertedString += 'o';
-                break;
-            case 'ź':
-                convertedString += 'z';
-                break;
-            case 'ż':
-                convertedString += 'z';
-                break;
+            case 'ą': convertedString += 'a'; break;
+            case 'ć': convertedString += 'c'; break;
+            case 'ę': convertedString += 'e'; break;
+            case 'ł': convertedString += 'l'; break;
+            case 'ń': convertedString += 'n'; break;
+            case 'ó': convertedString += 'o'; break;
+            case 'ś': convertedString += 's'; break;
+            case 'ó': convertedString += 'o'; break;
+            case 'ź': convertedString += 'z'; break;
+            case 'ż': convertedString += 'z'; break;
 
             default:
                 convertedString += raide;
@@ -60,6 +38,5 @@ function correctPolishLetters(string) {
 }
 
 console.log(correctPolishLetters("Jędrzej Błądziński"), '->', "Jedrzej Bladzinski");
-console.log(correctPolishLetters("Lech Śałęsa"), '->', "Lech Salesa");
 console.log(correctPolishLetters("Lech Wałęsa"), '->', "Lech Walesa");
 console.log(correctPolishLetters("Maria Skłodowska-Curie"), '->', "Maria Sklodowska-Curie");
