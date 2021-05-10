f62e3b63bb4d2e857bb541ac67387d6940ae4965
Rimantas <belauzas@yahoo.com>
2021-05-10 21:08:27 +0300

    Bricks and milk

--- random/bricks.js
+++ random/bricks.js
@@ -9,9 +9,23 @@ UZDUOTIS:
 */
 
 function bricks(sienuPlociai, sienosAukstis, plytosAukstis, plytosPlotis) {
+    let bendrasSienuPlotis = 0;
 
+    for (let i = 0; i < sienuPlociai.length; i++) {
+        const sienosPlotis = sienuPlociai[i];
+        bendrasSienuPlotis += sienosPlotis;
+    }
+
+    const plytuAukstyje = Math.ceil(sienosAukstis / plytosAukstis);
+    const plytuPlotyje = Math.ceil(bendrasSienuPlotis / plytosPlotis);
+
+    const plytuKiekis = plytuAukstyje * plytuPlotyje;
+
+    return plytuKiekis;
 }
 
 console.log(bricks([1, 2], 1, 0.1, 0.2), '->', 150);
 console.log(bricks([1, 1, 1], 1, 0.1, 0.2), '->', 150);
 console.log(bricks([2, 1, 3], 0.5, 0.1, 0.2), '->', 150);
+console.log(bricks([1], 1, 0.1, 0.2), '->', 50);
+console.log(bricks([1.05], 1.05, 0.1, 0.2), '->', 66);
