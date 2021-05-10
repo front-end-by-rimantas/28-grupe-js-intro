77fa13ce15bfcb0ece346f67a39e6de483e0e59a
Rimantas <belauzas@yahoo.com>
2021-05-10 19:54:23 +0300

    Bricks

---
+++ random/bricks.js
@@ -0,0 +1,17 @@
+/*
+UZDUOTIS:
+- gaunam, kiek sienu turi namas
+- gaunam, kokio plocio yra kiekviena siena (metrais)
+- sienos neturi angu ir/ar skyliu
+- gaunam sienos auksti, kuris visur yra vienodas
+- turim plytos dydi (aukstis ir plotis) (metrais)
+- reikia suzinoti, keliu plytu mums reikes, jei plytu skaldyti negalima
+*/
+
+function bricks(sienuPlociai, sienosAukstis, plytosAukstis, plytosPlotis) {
+
+}
+
+console.log(bricks([1, 2], 1, 0.1, 0.2), '->', 150);
+console.log(bricks([1, 1, 1], 1, 0.1, 0.2), '->', 150);
+console.log(bricks([2, 1, 3], 0.5, 0.1, 0.2), '->', 150);
